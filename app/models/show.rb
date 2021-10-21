74eb85a4ac91c288245de47375a493a1ac2727c2
Yewei <yeweichen1@gmail.com>
2021-10-20 20:05:59 -0500

    commit tree 1d2b7bbb118417305520dca2c58eeee486e2ee9a

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+
+  def actors_list
+    self.characters.actors_id.each do |actor|
+      actor.first_name + actor.last_name
+    end
+  end
 end
